d5b8b09ef1108349670beea466c0ea507132f925
מתן אלקיים <matan230595@gmail.com>
2026-02-11 02:33:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-651064298-5e520",
+  "appId": "1:165458461658:web:e824f788e52404df4036e3",
+  "apiKey": "AIzaSyBSlqGN9Wc9s6DcrnOv4MahiZvAcRN-TOc",
+  "authDomain": "studio-651064298-5e520.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "165458461658"
+};
